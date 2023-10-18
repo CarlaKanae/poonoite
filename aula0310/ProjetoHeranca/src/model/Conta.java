03606577a736b7c9e9507085a972d4839f04ca2a
Carla Oliveira <kanaestudio23@gmail.com>
2023-10-18 01:08:35 -0300

    Materia aula Maromo POO

--- aula0310/ProjetoHeranca/src/model/Conta.java
+++ aula0310/ProjetoHeranca/src/model/Conta.java
@@ -1,48 +1,59 @@
 package model;
 
-public abstract class Conta {
+public abstract class Conta 
+{
     private int numConta;
     private String nomeCliente;
     private double saldo;
 
     //Construtor personalizado
-
-    public Conta(int numConta, String nomeCliente, double saldo) {
+    public Conta(int numConta, String nomeCliente, double saldo) 
+    {
         this.numConta = numConta;
         this.nomeCliente = nomeCliente;
         this.saldo = saldo;
     }
-
-    public int getNumConta() {
+    
+    //Getters and Setters
+    public int getNumConta() 
+    {
         return numConta;
     }
 
-    public void setNumConta(int numConta) {
+    public void setNumConta(int numConta) 
+    {
         this.numConta = numConta;
     }
 
-    public String getNomeCliente() {
+    public String getNomeCliente() 
+    {
         return nomeCliente;
     }
 
-    public void setNomeCliente(String nomeCliente) {
+    public void setNomeCliente(String nomeCliente) 
+    {
         this.nomeCliente = nomeCliente;
     }
 
-    public double getSaldo() {
+    public double getSaldo() 
+    {
         return saldo;
     }
 
-    public void setSaldo(double saldo) {
+    public void setSaldo(double saldo) 
+    {
         this.saldo = saldo;
     }
 
-    public void depositar(double valor){
+    public void depositar(double valor)
+    {
         this.saldo += valor;
     }
 
-    public boolean sacar(double valor){
-        if(saldo >= valor){
+    public boolean sacar(double valor)
+    {
+        if(saldo >= valor)
+        {
             this.saldo -= valor;
             return true;
         }
@@ -50,7 +61,8 @@ public abstract class Conta {
     }
 
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return "Dados da conta " + numConta + "\n" +
                 "Nome Cliente: " + nomeCliente + "\n" +
                 "Saldo da Conta: " + saldo + "\n";
